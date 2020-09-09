48b78fcd4092490837155caafc7a7085a46a38f7
EduardoSanchezH25 <esanchezh@unsa.edu.pe>
2020-09-08 23:25:35 -0500

    Mensajes con tiempo de vida, uno por uno

--- public/chat.js
+++ public/chat.js
@@ -7,6 +7,8 @@ let btn = document.getElementById('send');
 let output = document.getElementById('output');
 let actions = document.getElementById('actions');
 
+let queue = [];
+
 btn.addEventListener('click', function(){
     socket.emit('chat:message', {
         username: username.value,
@@ -14,10 +16,35 @@ btn.addEventListener('click', function(){
     })
 })
 
-socket.on('chat:message', function(data){
-    output.innerHTML += `
+let i = 0;
+
+function debugg(){
+    console.log(i++);
+
+}
+
+function deleteFirstParrafo(){
+    debugg()
+    var primer_p = output.getElementsByTagName('p')[0];
+    output.removeChild(primer_p);
+    queue.shift();
+    if(queue.length > 0){
+        printParrafo(queue[0])
+    }
+}
+
+function printParrafo(data){
+    output.innerHTML = `
         <p>
             <strong>${data.username}</strong>: ${data.message}
         </p>
     `
+    setTimeout(deleteFirstParrafo, 3000);
+}
+
+socket.on('chat:message', function(data){
+    queue.push(data);
+    if(queue.length === 1){
+        printParrafo(data);
+    }
 })
